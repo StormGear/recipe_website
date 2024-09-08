d1794d71e37f84c5fc0972bfd695dcbcc90c3d2f
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-09-08 09:39:30 +0000

    writing some css for recipes section

--- js/app.js
+++ js/app.js
@@ -12,7 +12,16 @@ const navBtnDOM = getElement('.nav-btn')
 
 navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links')
+  if (window.innerWidth < 992 && links.classList.contains('show-links')) { 
+    const page = getElement('.page')
+    page.style.paddingTop = '2rem';
+  } else { 
+    const page = getElement('.page')
+    page.style.paddingTop = '0';
+  }
 })
+
+
 
 const date = getElement('#date')
 const currentYear = new Date().getFullYear()
