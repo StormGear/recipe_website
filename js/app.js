04e9ba381a954eaf17f2e6177ec6b43ff23f0a0c
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-09-13 17:05:56 +0000

    changed app logo

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 const getElement = (selector) => {
+  try {
   const element = document.querySelector(selector)
 
+  
   if (element) return element
   throw Error(
     `Please double check your class names, there is no ${selector} class`
   )
+} catch (error) { 
+  console.error(error)
+}
 }
 
 const links = getElement('.nav-links')
 const navBtnDOM = getElement('.nav-btn')
+// const btnBlock = getElement('.btn-block')
+
+// if (btnBlock) {
+// btnBlock.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log(e.type);
+// })
+// }
 
 navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links')
